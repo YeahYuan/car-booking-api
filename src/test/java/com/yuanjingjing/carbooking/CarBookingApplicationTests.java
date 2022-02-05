f1c1621eb73e658137902716b046482fb941b892
袁二 <yuaner@yuanerdeMacBook-Pro.local>
2022-02-05 11:36:48 +0800

    Initial commit

---
+++ src/test/java/com/yuanjingjing/carbooking/CarBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanjingjing.carbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
